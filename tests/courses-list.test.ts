2d564328846ccd1c751107942378dbf36ad9c832
svt <xkochevnikx@protonmail.com>
2025-02-09 11:32:14 +0300

    test(other): test update
    Changelog: test

--- tests/courses-list.test.ts
+++ tests/courses-list.test.ts
@@ -4,21 +4,14 @@ test("test", async ({ page }) => {
   console.log("Navigating to:", process.env.TEST_ENV_BASE_URL);
 
   await page.goto("/");
-
   await page.waitForLoadState("load"); // ✅ Гарантирует, что DOM загружен
   await page.getByRole("textbox", { name: "name" }).click();
-
   await page.getByRole("textbox", { name: "name" }).fill("svyat");
-  await page.getByRole("textbox", { name: "description" }).click();
+  await page.getByRole("textbox", { name: "name" }).press("Tab");
   await page
     .getByRole("textbox", { name: "description" })
     .fill("frontend engineer");
   await page.getByRole("button", { name: "Submit" }).click();
-  await page.getByText("svyat").click();
-  await page
-    .locator("div")
-    .filter({ hasText: /^svyat$/ })
-    .getByRole("button")
-    .click();
+  await page.getByRole("button", { name: "Удалить" }).click();
   await expect(page.getByText("svyat")).not.toBeVisible();
 });
