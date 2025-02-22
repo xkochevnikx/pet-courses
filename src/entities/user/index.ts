3f82380761e202b23f5076abb2ac0baa7f5bb701
svt <xkochevnikx@protonmail.com>
2025-02-22 11:16:06 +0300

    feat(root): added server-index for tree shaking
    Changelog: feature

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,3 +1,2 @@
 export { useAppSessionClient } from "./model/get-session-client";
-export { nextAuthConfig } from "./model/next-auth-config";
 export { SessionProvider } from "./model/session-provider";
---
+++ src/entities/user/server-index.ts
@@ -0,0 +1,1 @@
+export { nextAuthConfig } from "./model/next-auth-config";
