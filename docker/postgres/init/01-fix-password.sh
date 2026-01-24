fa0f557ea47022793e8c20717c4c0c8016f70235
Your Name <svt14@tokens.team>
2026-01-24 12:12:57 +0300

    build(root): update compose
    Changelog: build

--- docker/postgres/init/01-fix-password.sh
+++ docker/postgres/init/01-fix-password.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 set -e
 
-# Этот скрипт выполняется ТОЛЬКО при первом создании PGDATA (пустой volume).
-# POSTGRES_USER/POSTGRES_PASSWORD/POSTGRES_DB уже доступны как env.
-
-echo "[db-init] ensure password for role '${POSTGRES_USER}'"
+echo "[db-init] POSTGRES_USER=${POSTGRES_USER:-<empty>}"
+echo "[db-init] POSTGRES_DB=${POSTGRES_DB:-<empty>}"
+echo "[db-init] POSTGRES_PASSWORD is set? $([ -n "${POSTGRES_PASSWORD:-}" ] && echo yes || echo no)"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-SQL
   ALTER USER "${POSTGRES_USER}" WITH PASSWORD '${POSTGRES_PASSWORD}';
