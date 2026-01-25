d4c2a026f082168e28ee9387fa40dae417f11ee5
Your Name <svt14@tokens.team>
2026-01-25 18:47:56 +0300

    refactor(pages): auth page forse dynamic
    Changelog: refactor

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,9 @@ import { ReactNode } from "react";
 
 import { Header } from "@/widgets/header";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function Layout({ children }: { children: ReactNode }) {
   return (
     <>
